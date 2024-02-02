4300e5fa0deacb5b6edab7f755b647d722023220
Avneesh Mishra <123avneesh@gmail.com>
2024-02-02 16:12:21 +0530

    Minor documentation changes and added intersphinx for PyTorch and NumPy

--- docs/api/index.rst
+++ docs/api/index.rst
@@ -12,4 +12,4 @@ The main API of MLHub has the following modules
 
 - ``mlhub`` - The main module of MLHub (this page)
 - :py:mod:`mlhub.utils` - Utilities (functions) for the MLHub module
-- The API for included models can be found at :doc:`/api/models`. The detailed information pages can be found at :doc:`/models/index`.
+- :doc:`/api/models` - API for included models. Information pages at :doc:`/models/index`.
--- docs/conf.py
+++ docs/conf.py
@@ -235,4 +235,8 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # intersphinx_mapping = {'https://docs.python.org/': None}
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'pytorch': ('https://pytorch.org/docs/stable', None),
+    'numpy': ('https://numpy.org/doc/stable', None),
+}
